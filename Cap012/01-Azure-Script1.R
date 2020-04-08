cccd3f5cb0aa4ac14d641726a5940743a24901de
alexandret01 <alexandrecarmo.t@hotmail.com>
2020-04-07 22:41:52 -0300

    Fim

---
+++ Cap012/01-Azure-Script1.R
@@ -0,0 +1,33 @@
+#Diretório
+setwd("~/Documentos/BigDataAnalytics-R-Azure2.0/Cap012/")
+
+#Variável que controla a execução do script
+Azure <- FALSE
+
+#Execução de acordo com o valor da variável Azure
+if(Azure){
+  source("src/Tools.R")
+  Bikes <- maml.mapInputPort(1)
+  Bikes$dteday <- set.asPOSIXct(Bikes)
+}else{
+  source("Tools.R")
+  Bikes <- read.csv("datasets/bikes.csv", sep = ",", header = T, stringsAsFactors = F )
+  Bikes$dteday <- char.toPOSIXct(Bikes)
+}
+
+require(dplyr)
+print("Dimensões do dataframe antes das operações de transformação:")
+print(dim(Bikes))
+
+#Filtrando o dataframe
+Bikes <- Bikes %>% filter(cnt > 100)
+
+print("Dimensões do dataframe após as operações de transformação:")
+print(dim(Bikes))
+
+#ggplot2
+require(ggplot2)
+qplot(dteday, cnt, data = subset(Bikes, hr == 9), geom = "line")
+
+#Resultado
+if(Azure) maml.mapOutputPort("Bikes")
